497075eb1da95724aea1011e005699cf062a62b6
Ghalia-prog <79148102+Ghalia-prog@users.noreply.github.com>
2021-10-15 12:15:03 +0200

    Add files via upload

---
+++ mention.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+int y;
+int X;
+y=nb(%%0);
+printf("quel est la valeur de X ?\n") ;
+scanf("%f",&X) ;
+
+if (X=) {
+ printf("nombre parfait \n");
+}
+ 
+else if {
+ printf("Non parfait\n");
+}
+
+
